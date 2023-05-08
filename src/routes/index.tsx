5cfa7da7a3ad51de118556a7f454da49a4f2c313
Alfonso BT <ponchobt.dev@gmail.com>
2023-05-08 14:52:30 -0600

    Cool

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,16 @@ import { useRoutes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Stack from "@mui/material/Stack";
 import LinearProgress from "@mui/material/LinearProgress";
-import Home from "../pages/Lazy";
+import LazyHola from "../pages/LazyHola";
 import PadresHijos from "../pages/PadresHijos";
 import Page404 from "../pages/404";
 
-const Imagen = lazy(() => import("../pages/LazyRouter"));
+const LazyRouter = lazy(() => import("../pages/LazyRouter"));
 
 export default function Router() {
   let element = useRoutes([
     {
-      path: "home", element: (<div><Home /></div>),
+      path: "home", element: (<div><LazyHola /></div>),
     },
     {
       path: "padreshijos", element: (<div><PadresHijos /></div>),
@@ -31,7 +31,7 @@ export default function Router() {
             </div>
           }
         >
-          <Imagen />
+          <LazyRouter />
         </Suspense>
       ),
     },
