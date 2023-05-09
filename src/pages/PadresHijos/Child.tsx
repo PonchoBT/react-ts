351fab6d300065fd486d722a48eb05905d12672d
Alfonso BT <ponchobt.dev@gmail.com>
2023-05-08 18:21:16 -0600

    Papa y hijo

---
+++ src/pages/PadresHijos/Child.tsx
@@ -0,0 +1,10 @@
+function ChildComponent(props:any) {
+  return (
+    <div>
+      <h1>{props.hola}</h1>
+      <h1>{props.comoestas}</h1>
+    </div>
+  );
+}
+
+export default ChildComponent;
--- src/pages/PadresHijos/index.tsx
+++ src/pages/PadresHijos/index.tsx
@@ -1,55 +1,13 @@
-// import { useState } from 'react';
-
-// function PadresHijos() {
-//   // Se puede definir el state aquí
-//   const [name] = useState("Poncho Balderas Torres");
-
-//   // Parent component
-//   function Papa() {
-//     // Pasamos el state como prop al hijo
-//     return <Hijo name={name} />;
-//   }
-
-//   // Child component
-//   function Hijo(props: any) {
-//     return <p>Hello, {props.name}!</p>;
-//   }
-
-//   // Renderizamos el componente padre
-//   return (
-//     <div>
-//       <h1>Padres e hijos</h1>
-//       <Papa />
-//     </div>
-//   );
-// };
-
-// export default PadresHijos;
-
-import React, { useState } from "react";
+import ChildComponent from "./Child";
 
 function PadresHijos() {
-  const [name, setName] = useState("John");
+  const greeting = "Hola Alfonso";
+  const greetings = "Yo bien y ¿tu?";
 
   return (
     <div>
-      <h1>Hello from Parent component</h1>
-      <Child name={name} handleClick={() => setName("Jane")} />
-    </div>
-  );
-}
-
-interface ChildProps {
-  name: string;
-  handleClick: () => void;
-}
-
-function Child({ name, handleClick }: ChildProps) {
-  return (
-    <div>
-      <h2>Hello from Child component</h2>
-      <p>The name is {name}</p>
-      <button onClick={handleClick}>Change name</button>
+      <ChildComponent hola={greeting} />
+      <ChildComponent comoestas={greetings} />
     </div>
   );
 }
