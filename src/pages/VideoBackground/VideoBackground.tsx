eadbf58347906fd05ac091b426eb9ad636bb762b
Alfonso BT <ponchobt.dev@gmail.com>
2023-05-21 21:03:10 -0600

    Video

--- src/pages/VideoBackground/VideoBackground.tsx
+++ src/pages/VideoBackground/VideoBackground.tsx
@@ -1,27 +1,48 @@
-import React from "react";
-import ReactPlayer from "react-player";
 import "./videobackground.css";
 
+import { useState } from "react";
 
-const VideoBackground: React.FC = () => {
+import Vimeo from "@u-wave/react-vimeo";
+
+export default function App() {
+  const [isVimeoSource, setIsVimeoSource] = useState(false);
+
   return (
-    <div className="video-container">
-      <ReactPlayer
-        url="https://frontendfactored.s3.us-west-1.amazonaws.com/video.mp4" // Reemplaza "ruta-del-video.mp4" con la ruta real de tu video
-        playing
-        loop
-        muted
-        width="100%"
-        height="100%"
-        style={{ position: "absolute", top: 0, left: 0 }}
-        playsinline
-      />
-      <div className="content">
-        <h1>Bienvenido a mi sitio web</h1>
-        <p>Este es un ejemplo de fondo de video en React con TypeScript.</p>
+    <div className="full-screen-container">
+      <div className="label-text">
+        {isVimeoSource ? "Vimeo iFrame" : "HTML Video Element"}
       </div>
+      <label className="switch">
+        <input
+          type="checkbox"
+          onClick={() => setIsVimeoSource((prev) => !prev)}
+        />
+        <span className="slider round" />
+      </label>
+
+      {isVimeoSource ? (
+        <Vimeo
+          className="react-vimeo-player"
+          video={"736830436"}
+          // width={"100%"}
+          //height={"100%"}
+          autoplay={true}
+          muted={true}
+          loop={true}
+          responsive={true}
+          // controls={false}
+          background={true}
+        />
+      ) : (
+        <video className="full-screen-video" playsInline autoPlay muted loop>
+          <source
+            src={
+              "https://res.cloudinary.com/dlf5gecrf/video/upload/v1658606508/benjaminkratzin-website/BenjaminKratzin_DirectorReel_220722_Desktop_858p_4Mbs_v01_g2ufkx.mp4"
+            }
+            type="video/mp4"
+          />
+        </video>
+      )}
     </div>
   );
-};
-
-export default VideoBackground;
+}
