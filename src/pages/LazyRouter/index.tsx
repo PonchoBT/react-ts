5093906cd4e97987c5a48e284fe8dd8d534bf72c
PonchoBT <ponchobt.dev@gmail.com>
2025-05-20 12:00:00 -0600

    Contribución del 20 de mayo 2025

--- src/pages/LazyRouter/index.tsx
+++ src/pages/LazyRouter/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Logo from "../LazyRouter/logo.png";
+import { Grid, Typography, Paper } from "@mui/material";
+
+const codeString = `
+import React from "react";
+import Logo from "../LazyRouter/logo.png";
 
 const Imagen: React.FC = () => {
   return (
@@ -10,3 +15,57 @@ const Imagen: React.FC = () => {
 };
 
 export default Imagen;
+`;
+
+const Imagen: React.FC = () => {
+  return (
+    <Grid container spacing={4}>
+      {/* Demo Section */}
+      <Grid item xs={12} md={6}>
+        <Typography variant="h5" gutterBottom>
+          Demo
+        </Typography>
+        <div>
+          <img src={Logo} alt="Mi imagen"/>
+        </div>
+      </Grid>
+
+      {/* Code Section */}
+      <Grid item xs={12} md={6}>
+        <Typography variant="h5" gutterBottom>
+          Code
+        </Typography>
+        <Paper 
+          elevation={3}
+          sx={{ 
+            p: 2,
+            backgroundColor: '#f5f5f5',
+            maxHeight: '600px',
+            overflow: 'auto'
+          }}
+        >
+          <pre style={{ 
+            margin: 0,
+            whiteSpace: 'pre-wrap',
+            wordWrap: 'break-word'
+          }}>
+            {codeString}
+          </pre>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Imagen;
+
+
+
+
+
+
+
+
+
+
+
